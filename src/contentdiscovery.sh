b41bdb5ef71bdea309b7f27c10a033b538a0fa0b
Anon-Artist <61599526+Anon-Artist@users.noreply.github.com>
2020-10-24 18:34:52 +0530

    Update contentdiscovery.sh

--- src/contentdiscovery.sh
+++ src/contentdiscovery.sh
@@ -54,13 +54,14 @@ echo " "
 if [ -d ~/tools/dirsearch/ ]
 then
   echo "${magenta} [+]  Content Discovery ${reset}"
-  python3 ~/tools/dirsearch/dirsearch.py -u https://$DOM -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,zip,sql -w ~/tools/wordlists/common.txt -x 429,400,503 -f --random-agents -t 300 -L ~/recon/$DOM/Content_discovery/directories.txt --max-retries=3 -b -r
+  python3 ~/tools/dirsearch/dirsearch.py -u https://$DOM -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,zip,sql -w ~/tools/wordlists/common.txt -x 429,400,503 -f --random-agents -t 300 --max-retries=3 -b -r
 else
   echo "${blue} [+] Installing dirsearch ${reset}"
   git clone https://github.com/maurosoria/dirsearch ~/tools/dirsearch
   wget https://raw.githubusercontent.com/v0re/dirb/master/wordlists/common.txt -P ~/tools/wordlists
   echo "${magenta} [+] Running Content Discovery ${reset}"
-  python3 ~/tools/dirsearch/dirsearch.py -u https://$DOM -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,zip,sql -w ~/tools/wordlists/common.txt -x 429,400,503 -f --random-agents -t 300 -L ~/recon/$DOM/Content_discovery/directories.txt --max-retries=3 -b -r
+  python3 ~/tools/dirsearch/dirsearch.py -u https://$DOM -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,zip,sql -w ~/tools/wordlists/common.txt -x 429,400,503 -f --random-agents -t 300 --max-retries=3 -b -r
+  mv ~/tools/dirsearch/reports/$DOM/* ~/recon/$DOM/Content_discovery/dirsearch_result.txt
 fi
 echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
 echo " "
