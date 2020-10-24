b8ff985b3269c362354c5cc6a9404cdaa6e80fb3
Anon-Artist <61599526+Anon-Artist@users.noreply.github.com>
2020-10-24 19:03:02 +0530

    Update contentdiscovery.sh

--- src/contentdiscovery.sh
+++ src/contentdiscovery.sh
@@ -54,14 +54,31 @@ echo " "
 if [ -d ~/tools/dirsearch/ ]
 then
   echo "${magenta} [+]  Content Discovery ${reset}"
-  python3 ~/tools/dirsearch/dirsearch.py -u https://$DOM -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,zip,sql -w ~/tools/wordlists/common.txt -x 429,400,503 -f --random-agents -t 300 --max-retries=3 -b -r
+  for url in $(cat ~/recon/$DOM/all-alive-subs.txt);do
+  reg=$(echo $url | sed -e 's;https\?://;;' | sed -e 's;/.*$;;')
+  python3 ~/tools/dirsearch/dirsearch.py -u $url -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,zip,sql -w ~/tools/wordlists/common.txt -x 429,400,503 -f --random-agents -t 300 --max-retries=3 -b -r --plain-text-report=~/tools/dirsearch/$reg.tmp
+  if [ ! -s ~/tools/dirsearch/$reg.tmp ]; then
+            rm ~/tools/dirsearch/$reg.tmp
+        else
+            cat ~/tools/dirsearch/$reg.tmp | sort -k 1 -n > ~/tools/dirsearch/$reg.txt
+            rm ~/tools/dirsearch/$reg.tmp
+        fi
+    done
 else
   echo "${blue} [+] Installing dirsearch ${reset}"
   git clone https://github.com/maurosoria/dirsearch ~/tools/dirsearch
   wget https://raw.githubusercontent.com/v0re/dirb/master/wordlists/common.txt -P ~/tools/wordlists
   echo "${magenta} [+] Running Content Discovery ${reset}"
-  python3 ~/tools/dirsearch/dirsearch.py -u https://$DOM -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,zip,sql -w ~/tools/wordlists/common.txt -x 429,400,503 -f --random-agents -t 300 --max-retries=3 -b -r
-  mv ~/tools/dirsearch/reports/$DOM/* ~/recon/$DOM/Content_discovery/dirsearch_result.txt
+  for url in $(cat ~/recon/$DOM/all-alive-subs.txt);do
+  reg=$(echo $url | sed -e 's;https\?://;;' | sed -e 's;/.*$;;')
+  python3 ~/tools/dirsearch/dirsearch.py -u $url -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,zip,sql -w ~/tools/wordlists/common.txt -x 429,400,503 -f --random-agents -t 300 --max-retries=3 -b -r --plain-text-report=~/tools/dirsearch/$reg.tmp
+  if [ ! -s ~/tools/dirsearch/$reg.tmp ]; then
+            rm ~/tools/dirsearch/$reg.tmp
+        else
+            cat ~/tools/dirsearch/$reg.tmp | sort -k 1 -n > ~/tools/dirsearch/$reg.txt
+            rm ~/tools/dirsearch/$reg.tmp
+        fi
+    done
 fi
 echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
 echo " "
