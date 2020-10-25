8b0cb8bc68e1dd231f9443b6f27b8100f8328ded
Anon-Artist <61599526+Anon-Artist@users.noreply.github.com>
2020-10-25 21:52:45 +0530

    Update contentdiscovery.sh

--- src/contentdiscovery.sh
+++ src/contentdiscovery.sh
@@ -10,27 +10,33 @@ reset=`tput sgr0`
  
 read -p "Enter domain name : " DOM
  
-if [ -d ~/recon/ ]
+if [ -d ~/reconizer/ ]
 then
   echo " "
 else
-  mkdir ~/recon
+  mkdir ~/reconizer
+fi
+ 
+if [ -d ~/reconizer/tools ]
+then
+  echo " "
+else
+  mkdir ~/reconizer/tools 
+fi
+
+if [ -d ~/reconizer/$DOM ]
+then
+  echo " "
+else
+  mkdir ~/reconizer/$DOM
  
 fi
  
-if [ -d ~/recon/$DOM ]
+if [ -d ~/reconizer/$DOM/Content_Discovery ]
 then
   echo " "
 else
-  mkdir ~/recon/$DOM
- 
-fi
- 
-if [ -d ~/recon/$DOM/feroxbuster ]
-then
-  echo " "
-else
-  mkdir ~/recon/$DOM/feroxbuster
+  mkdir ~/reconizer/$DOM/Content_Discovery
  
 fi
  
@@ -54,38 +60,38 @@ echo " "
 if [ -f /usr/bin/feroxbuster ]
 then
  echo "${magenta} [+] Running Feroxbuster ${reset}"
- for url in $(cat ~/recon/$DOM/all-alive-subs.txt);do
+ for url in $(cat ~/reconizer/$DOM/Subdomains/all-alive-subs.txt);do
  reg=$(echo $url | sed -e 's;https\?://;;' | sed -e 's;/.*$;;')
- feroxbuster --url $url -w ~/tools/wordlists/common.txt -x php asp aspx jsp py txt conf config bak backup swp old db zip sql --threads 300 --output ~/recon/$DOM/Content_Discovery/content_discovery_result.txt
+ feroxbuster --url $url -w ~/reconizer/tools/common.txt -x php asp aspx jsp py txt conf config bak backup swp old db zip sql --threads 300 --output ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt
 done
 else
  echo "${blue} [+] Installing Feroxbuster ${reset}"
- wget https://raw.githubusercontent.com/v0re/dirb/master/wordlists/common.txt -P ~/tools/wordlists
- wget https://github.com/epi052/feroxbuster/releases/latest/download/feroxbuster_amd64.deb.zip -P ~/tools/feroxbuster
- unzip ~/tools/feroxbuster/feroxbuster_amd64.deb.zip -d ~/tools/feroxbuster 
- sudo apt install ~/tools/feroxbuster/*.deb
+ wget https://raw.githubusercontent.com/v0re/dirb/master/wordlists/common.txt -P ~/reconizer/tools/
+ wget https://github.com/epi052/feroxbuster/releases/latest/download/feroxbuster_amd64.deb.zip -P ~/reconizer/tools/feroxbuster
+ unzip ~/reconizer/tools/feroxbuster/feroxbuster_amd64.deb.zip -d ~/reconizer/tools/feroxbuster 
+ sudo apt install ~/reconizer/tools/feroxbuster/*.deb
  echo "${magenta} [+] Running Feroxbuster ${reset}"
- for url in $(cat ~/recon/$DOM/all-alive-subs.txt);do
+ for url in $(cat ~/reconizer/$DOM/Subdomains/all-alive-subs.txt);do
  reg=$(echo $url | sed -e 's;https\?://;;' | sed -e 's;/.*$;;')
- feroxbuster --url $url -w ~/tools/wordlists/common.txt -x php asp aspx jsp py txt conf config bak backup swp old db zip sql --threads 300 --output ~/recon/$DOM/Content_Discovery/content_discovery_result.txt
+ feroxbuster --url $url -w ~/reconizer/tools/common.txt -x php asp aspx jsp py txt conf config bak backup swp old db zip sql --threads 300 --output ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt
 echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
 echo " "
-echo "${blue} [+] Succesfully saved to content_discovery_result.txt ${reset}"
+echo "${blue} [+] Succesfully saved as content_discovery_result.txt ${reset}"
 echo " "
 echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
 echo " "
 echo "${magenta} [+] Sorting According to Status Codes ${reset}"
-cat content_discovery_result | grep 200 | awk '{print $2}' > status_code_200.txt  
-cat content_discovery_result | grep 204 | awk '{print $2}' > status_code_204.txt
-cat content_discovery_result | grep 301 | awk '{print $2}' > status_code_301.txt
-cat content_discovery_result | grep 302 | awk '{print $2}' > status_code_302.txt
-cat content_discovery_result | grep 307 | awk '{print $2}' > status_code_307.txt
-cat content_discovery_result | grep 308 | awk '{print $2}' > status_code_308.txt
-cat content_discovery_result | grep 401 | awk '{print $2}' > status_code_401.txt
-cat content_discovery_result | grep 403 | awk '{print $2}' > status_code_403.txt
-cat content_discovery_result | grep 405 | awk '{print $2}' > status_code_405.txt
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 200 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_200.txt  
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 204 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_204.txt
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 301 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_301.txt
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 302 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_302.txt
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 307 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_307.txt
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 308 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_308.txt
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 401 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_401.txt
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 403 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_403.txt
+cat ~/reconizer/$DOM/Content_Discovery/content_discovery_result.txt | grep 405 | awk '{print $2}' > ~/reconizer/$DOM/Content_Discovery/status_code_405.txt
 echo " "
-echo "${blue} [+] Succesfully saved According to status Codes ${reset}"
+echo "${blue} [+] Succesfully saved according to status codes ${reset}"
 echo " "
 done
 fi
