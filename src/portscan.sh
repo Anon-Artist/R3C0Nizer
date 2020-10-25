c5ac7cc65cf51d4711e647851e7a2fd8e4ad040c
Anon-Artist <61599526+Anon-Artist@users.noreply.github.com>
2020-10-25 19:54:22 +0530

    Create portscan.sh

---
+++ src/portscan.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+ 
+#colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+reset=`tput sgr0`
+ 
+read -p "Enter domain name : " DOM
+ 
+if [ -d ~/recon/ ]
+then
+  echo " "
+else
+  mkdir ~/recon
+ 
+fi
+ 
+if [ -d ~/recon/$DOM ]
+then
+  echo " "
+else
+  mkdir ~/recon/$DOM
+ 
+fi
+ 
+if [ -d ~/recon/$DOM/Port_Scan ]
+then
+  echo " "
+else
+  mkdir ~/recon/$DOM/Port_Scan
+ 
+fi
+ 
+ 
+echo "${red}
+ =================================================
+|   ____  _____  ____ ___  _   _ _                |
+|  |  _ \|___ / / ___/ _ \| \ | (_)_______ _ __   |
+|  | |_) | |_ \| |  | | | |  \| | |_  / _ \ '__|  |
+|  |  _ < ___) | |__| |_| | |\  | |/ /  __/ |     |
+|  |_| \_\____/ \____\___/|_| \_|_/___\___|_|     |
+|                                                 |
+ ================== Anon-Artist ==================
+${reset}"
+echo "${blue} [+] Started Port Scanning ${reset}"
+echo " "
+ 
+#nuclei
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${magenta} [+] Running Rust Scan ${reset}"
+for url in $(cat ~/recon/$DOM/unique.txt);do
+sudo docker run -it --rm --name rustscan rustscan/rustscan:1.10.0 $url -b 4000 -u 5000 --ports  2075,2076,6443,3868,3366,8443,8080,9443,9091,3000,8000,5900,8081,6000,10000,8181,3306,5000,4000,8888,5432,15672,9999,161,4044,7077,4040,9000,8089,7447,7080,8880,8983,5673,7443,19000,19080,9200  | tee ~/recon/$DOM/Port_Scan/$url.txt
+done
+sed -i -n '/Starting Nmap 7.80/,$p' *.txt
+echo " "
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${blue} [+] Succesfully saved the results ${reset}"
+echo " "
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${red} [+] Thank you for using R3C0nizer${reset}"
+echo ""
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
