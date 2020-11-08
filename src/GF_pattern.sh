c6d3fb8e37ff4832ceada380093ce468c5170934
Anon-Artist <61599526+Anon-Artist@users.noreply.github.com>
2020-11-08 14:39:16 +0000

    Create GF_pattern.sh

---
+++ src/GF_pattern.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+
+#colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+reset=`tput sgr0`
+
+read -p "Enter domain name : " DOM
+
+if [ -d ~/reconizer/ ]
+then
+  echo " "
+else
+  mkdir ~/reconizer
+fi
+
+if [ -d ~/reconizer/tools ]
+then
+  echo " "
+else
+  mkdir ~/reconizer/tools 
+fi
+
+if [ -d ~/reconizer/$DOM/Archivescan ]
+then
+  echo " "
+else
+  mkdir ~/reconizer/$DOM/Archivescan
+fi
+
+if [ -d ~/reconizer/$DOM/GF_Patterns ]
+then
+  echo " "
+else
+  mkdir ~/reconizer/$DOM/GF_Patterns
+fi
+
+echo "${red}
+ =================================================
+|   ____  _____  ____ ___  _   _ _                |
+|  |  _ \|___ / / ___/ _ \| \ | (_)_______ _ __   |
+|  | |_) | |_ \| |  | | | |  \| | |_  / _ \ '__|  |
+|  |  _ < ___) | |__| |_| | |\  | |/ /  __/ |     |
+|  |_| \_\____/ \____\___/|_| \_|_/___\___|_|     |
+|                                                 |
+ ================== Anon-Artist ==================
+${reset}"
+echo "${blue} [+] Started URL Fetching ${reset}"
+echo " "
+
+#wayback_URL
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+if [ -f ~/go/bin/waybackurls ]
+then
+ echo "${magenta} [+] Running Waybackurls ${reset}"
+ cat  ~/reconizer/$DOM/Subdomains/all-alive-subs.txt | waybackurls >> ~/reconizer/$DOM/Archivescan/waybackurls.txt 
+else
+ echo "${blue} [+] Installing Waybackurls ${reset}"
+ go get -u github.com/tomnomnom/waybackurls
+ echo "${blue} [+] Started URL Fetching ${reset}"
+ cat  ~/reconizer/$DOM/Subdomains/all-alive-subs.txt | waybackurls >> ~/reconizer/$DOM/Archivescan/waybackurls.txt 
+fi
+echo " "
+echo "${blue} [+] Succesfully saved as waybackurls.txt ${reset}"
+echo " "
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+
+#Gau
+if [ -f ~/go/bin/gau ]
+then
+ echo "${magenta} [+] Running Gau ${reset}"
+ cat  ~/reconizer/$DOM/Subdomains/all-alive-subs.txt | gau  >> ~/reconizer/$DOM/Archivescan/gau.txt
+else
+ echo "${blue} [+] Installing Gaus ${reset}"
+ go get -u github.com/lc/gau
+ echo "${blue} [+] Started URL Fetching ${reset}"
+ cat  ~/reconizer/$DOM/Subdomains/all-alive-subs.txt | gau >> ~/reconizer/$DOM/Archivescan/gau.txt
+fi
+echo " "
+echo "${blue} [+] Succesfully saved as gau.txt ${reset}"
+echo " "
+
+#uniquesubdomains
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${blue} [+] fetching unique URL ${reset}"
+echo " "
+cat ~/reconizer/$DOM/Archivescan/waybackurls.txt ~/reconizer/$DOM/Archivescan/gau.txt | sort -u >> ~/reconizer/$DOM/Archivescan/sorted.txt
+echo "${blue} [+] Succesfully saved as sorted.txt ${reset}"
+echo " "
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+
+#GFPattern
+if [ -f ~/go/bin/gf ]
+then
+ echo "${magenta} [+] Running GF_Pattern ${reset}"
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf cors  >> ~/reconizer/$DOM/GF_Pattern/cors.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf aws-keys  >> ~/reconizer/$DOM/GF_Pattern/aws-keys.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf base64  >> ~/reconizer/$DOM/GF_Pattern/base64.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf s3-buckets >> ~/reconizer/$DOM/GF_Pattern/s3-buckets.txt
+else
+ echo "${blue} [+] Installing GF_Pattern ${reset}"
+ go get -u github.com/tomnomnom/gf
+ git clone https://github.com/1ndianl33t/Gf-Patterns
+ mkdir ~/.gf
+ mv ~/Gf-Patterns/*.json ~/.gf
+ cp ~/go/src/github.com/tomnomnom/gf/examples/*.json ~/.gf
+ echo "${blue} [+] Started GF_Pattern ${reset}"
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf cors | tee -a ~/reconizer/$DOM/GF_Patterns/cors.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf aws-keys | tee -a ~/reconizer/$DOM/GF_Patterns/aws-keys.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf base64 | tee -a ~/reconizer/$DOM/GF_Patterns/base64.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf s3-buckets | tee -a ~/reconizer/$DOM/GF_Patterns/s3-buckets.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf idor | tee -a ~/reconizer/$DOM/GF_Patterns/idor.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf lfi | tee -a ~/reconizer/$DOM/GF_Patterns/lfi.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf rce | tee -a ~/reconizer/$DOM/GF_Patterns/rce.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf redirect | tee -a ~/reconizer/$DOM/GF_Patterns/redirect.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf sqli | tee -a ~/reconizer/$DOM/GF_Patterns/sqli.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf ssrf | tee -a ~/reconizer/$DOM/GF_Patterns/ssrf.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf ssti | tee -a ~/reconizer/$DOM/GF_Patterns/ssti.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf interestingparams | tee -a ~/reconizer/$DOM/GF_Patterns/interesting_parameters.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf interestingsubs | tee -a ~/reconizer/$DOM/GF_Patterns/interesting_subs.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf xss | tee -a ~/reconizer/$DOM/GF_Patterns/xss.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf interestingEXT | tee -a ~/reconizer/$DOM/GF_Patterns/interesting_extensions.txt
+ cat  ~/reconizer/$DOM/Archivescan/sorted.txt | gf debug_logic | tee -a ~/reconizer/$DOM/GF_Patterns/debug_logic.txt
+fi
+echo " "
+echo "${blue} [+] Succesfully Finished GF_Pattern Scan ${reset}"
+echo " "
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${red} [+] Thank you for using R3C0nizer${reset}"
+echo ""
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
