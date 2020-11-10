8cc60a88b8dd017f903e40d02bef70e8ccd16848
Anon-Artist <61599526+Anon-Artist@users.noreply.github.com>
2020-11-10 10:17:33 +0000

    Update subenum.sh

--- src/subenum.sh
+++ src/subenum.sh
@@ -56,12 +56,12 @@ echo "${yellow} ---------------------------------- xxxxxxxx --------------------
 echo " "
 if [ -f ~/go/bin/assetfinder ]
 then
-  echo "${magenta} [+] Running Assetfinder ${reset}"
+  echo "${magenta} [+] Running Assetfinder for subdomain enumeration${reset}"
   assetfinder -subs-only $DOM  >> ~/reconizer/$DOM/Subdomains/assetfinder.txt 
 else
   echo "${blue} [+] Installing Assetfinder ${reset}"
   go get -u github.com/tomnomnom/assetfinder
-  echo "${magenta} [+] Running Assetfinder ${reset}"
+  echo "${magenta} [+] Running Assetfinder for subdomain enumeration${reset}"
   assetfinder -subs-only $DOM  >> ~/reconizer/$DOM/Subdomains/assetfinder.txt
 fi
 echo " "
@@ -73,13 +73,13 @@ echo "${yellow} ---------------------------------- xxxxxxxx --------------------
 echo " "
 if [ -f ~/go/bin/amass ]
 then
-  echo "${magenta} [+] Running Amass ${reset}"
+  echo "${magenta} [+] Running Amass for subdomain enumeration${reset}"
   amass enum --passive -d $DOM > ~/reconizer/$DOM/Subdomains/amass.txt
 else
   echo "${blue} [+] Installing Amass ${reset}"
   echo "${blue} [+] This may take few minutes hang tight... ${reset}"
   go get -u github.com/OWASP/Amass/...
-  echo "${magenta} [+] Running Amass ${reset}"
+  echo "${magenta} [+] Running Amass for subdomain enumeration${reset}"
   amass enum --passive -d $DOM > ~/reconizer/$DOM/Subdomains/amass.txt
 fi
 echo " "
@@ -91,12 +91,12 @@ echo "${yellow} ---------------------------------- xxxxxxxx --------------------
 echo " "
 if [ -f ~/go/bin/subfinder ]
 then
-  echo "${magenta} [+] Running Subfinder ${reset}"
+  echo "${magenta} [+] Running Subfinder for subdomain enumeration${reset}"
   subfinder -d $DOM -o ~/reconizer/$DOM/Subdomains/subfinder.txt 
 else
   echo "${blue} [+] Installing Subfinder ${reset}"
   go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
-  echo "${magenta} [+] Running Subfinder ${reset}"
+  echo "${magenta} [+] Running Subfinder for subdomain enumeration${reset}"
   subfinder -d $DOM -o ~/reconizer/$DOM/Subdomains/subfinder.txt
 fi
 echo " "
@@ -108,13 +108,13 @@ echo "${yellow} ---------------------------------- xxxxxxxx --------------------
 echo " "
 if [ -f ~/go/bin/findomain-linux ]
 then
-  echo "${magenta} [+] Running Findomain ${reset}"
+  echo "${magenta} [+] Running Findomain for subdomain enumeration${reset}"
     findomain-linux --target $DOM -u ~/reconizer/$DOM/Subdomains/findomain.txt
 else
   echo "${blue} [+] Installing Findomain ${reset}"
   wget https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux -P ~/go/bin/
   chmod +x ~/go/bin/findomain-linux
-  echo "${magenta} [+] Running Findomain ${reset}"
+  echo "${magenta} [+] Running Findomain for subdomain enumeration${reset}"
   findomain-linux --target $DOM -u ~/reconizer/$DOM/Subdomains/findomain.txt
 fi
 echo " "
@@ -126,11 +126,11 @@ echo "${yellow} ---------------------------------- xxxxxxxx --------------------
 echo " "
 if [ -d ~/reconizer/tools/Sublist3r/ ]
 then
-  echo "${magenta} [+] Running Sublist3r ${reset}"
+  echo "${magenta} [+] Running Sublist3r for subdomain enumeration${reset}"
   python ~/reconizer/tools/Sublist3r/sublist3r.py -d $DOM -t 10 -v -o ~/reconizer/$DOM/Subdomains/sublist3r.txt > /dev/null
 else
   echo "${blue} [+] Installing Sublist3r ${reset}"
-  echo "${magenta} [+] Running Sublist3r ${reset}"
+  echo "${magenta} [+] Running Sublist3r for subdomain enumeration${reset}"
   git clone https://github.com/aboul3la/Sublist3r.git ~/reconizer/tools/Sublist3r/
   python ~/reconizer/tools/Sublist3r/sublist3r.py -d $DOM -t 10 -v -o ~/reconizer/$DOM/Subdomains/sublist3r.txt > /dev/null
 fi
@@ -152,12 +152,12 @@ echo "${yellow} ---------------------------------- xxxxxxxx --------------------
 echo " "
 if [ -f ~/go/bin/httpx ]
 then
-  echo "${magenta} [+] Sorting Alive Subdomains ${reset}"
+  echo "${magenta} [+] Running Httpx for sorting alive subdomains${reset}"
 cat ~/reconizer/$DOM/Subdomains/unique.txt | httpx >> ~/reconizer/$DOM/Subdomains/all-alive-subs.txt
 else
   echo "${blue} [+] Installing Httpx ${reset}"
   go get -u github.com/projectdiscovery/httpx/cmd/httpx
-  echo "${magenta} [+] Sorting Alive Subdomains ${reset}"
+  echo "${magenta} [+] Running Httpx for sorting alive subdomains${reset}"
   cat ~/reconizer/$DOM/Subdomains/unique.txt | httpx >> ~/reconizer/$DOM/Subdomains/all-alive-subs.txt
 fi
 echo " "
